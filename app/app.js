0326ad12a37048793189e1cae96245e5d282b5da
nsecrist <secrist24@gmail.com>
2017-01-27 11:43:28 -0600

    Added HTML5 mode, still unable to navigate to section htmls

--- app/app.js
+++ app/app.js
@@ -2,35 +2,37 @@
 
 // Declare app level module which depends on views, and components
 var ngHelpApp = angular.module('ngHelp', [
-  'ngRoute',
-  'ngHelp.contents'
+    'ngRoute',
+    'ngHelp.contents'
 ]).
 
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-      $routeProvider.when('/home', {
-          templateUrl: '/partials/welcome.html'
-        }).
-      when('/p/:name*', {
-            templateUrl: function(urlattr){
-                return '/sections/' + urlattr.name;
-            },
-            controller: 'SectionController'
-        }).
-        otherwise({
-          redirectTo: '/home'
-        });
+    $routeProvider.when('/home', {
+        templateUrl: '/partials/welcome.html'
+    }).
+    when('/p/:name*', {
+        templateUrl: function(urlattr) {
+            return '/sections/' + urlattr.name;
+        },
+        controller: 'SectionController'
+    }).
+    otherwise({
+        redirectTo: '/home'
+    });
 
-        // $locationProvider.html5Mode({
-        //   enabled: true
-        // });
-        //
-        // $locationProvider.hashPrefix('!');
-    }
-]);
+    // Use the HTML5 History API
+    $locationProvider.html5Mode(true);
+
+    // $locationProvider.html5Mode({
+    //   enabled: true
+    // });
+    //
+    // $locationProvider.hashPrefix('!');
+}]);
 
 ngHelpApp.run(function($rootScope, $location, $anchorScroll, $routeParams) {
-  $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
-    $location.hash($routeParams.scrollTo);
-    $anchorScroll();
-  });
+    $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+        $location.hash($routeParams.scrollTo);
+        $anchorScroll();
+    });
 });
--- app/main.js
+++ app/main.js
@@ -9,64 +9,70 @@ const BrowserWindow = electron.BrowserWindow
 const app = electron.app
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
- if (process.platform != 'darwin') {
-   app.quit();
- }
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function () {
+app.on('ready', function() {
 
- // Create the browser window.
- mainWindow = new BrowserWindow({
-   width: 1024,
-   height: 768,
-   toolbar: false,
-   webPreferences: {
-     nodeIntegration: false
-   }
-  });
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 768,
+        toolbar: false,
+        webPreferences: {
+            nodeIntegration: false
+        }
+    });
 
-  mainWindow.setMenu(null);
+    mainWindow.setMenu(null);
 
-  mainWindow.maximize();
+    mainWindow.maximize();
 
-  var server = http.createServer(requestHandler).listen(9527);
+    var server = http.createServer(requestHandler).listen(9527);
 
- // and load the index.html of the app.
- mainWindow.loadURL('http://localhost:9527/index.html');
+    // and load the index.html of the app.
+    mainWindow.loadURL('http://localhost:9527/index.html');
 
- mainWindow.openDevTools();
+    mainWindow.openDevTools();
 
- // Emitted when the window is closed.
- mainWindow.on('closed', function () {
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
 
-   // Dereference the window object, usually you would store windows
-   // in an array if your app supports multi windows, this is the time
-   // when you should delete the corresponding element.
-   mainWindow = null;
- });
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 });
 
 function requestHandler(req, res) {
     var
-        file    = req.url == '/' ? '/index.html' : req.url,
-        root    = __dirname,
-        page404 = root + '/404.html';
+        file = req.url == '/' ? '/index.html' : req.url,
+        root = __dirname,
+        page404 = root + '/404.html',
+        fullPath = "";
 
-   console.log("root = " + root);
-   console.log("file = " + file);
-    getFile((root + file), res, page404);
+
+    console.log("root = " + root);
+    console.log("file = " + file);
+
+    // Make full path to file and replace / with \
+    fullPath = path.join(root, file).replace(/\//g, "/");
+    console.log("fullPath = " + fullPath);
+    getFile(fullPath, res, page404);
 };
 
 function getFile(filePath, res, page404) {
 
     fs.exists(filePath, function(exists) {
-        if(exists) {
+        if (exists) {
             fs.readFile(filePath, function(err, contents) {
-                if(!err) {
+                if (!err) {
                     res.end(contents);
                 } else {
                     console.dir(err);
@@ -74,8 +80,10 @@ function getFile(filePath, res, page404) {
             });
         } else {
             fs.readFile(page404, function(err, contents) {
-                if(!err) {
-                    res.writeHead(404, {'Content-Type': 'text/html'});
+                if (!err) {
+                    res.writeHead(404, {
+                        'Content-Type': 'text/html'
+                    });
                     res.end(contents);
                 } else {
                     console.dir(err);
