2764f2eb6578dbf6709c522bb99e2854731fcb50
nsecrist <secrist24@gmail.com>
2017-02-01 15:31:37 -0600

    Electron app can now be launched to open and display a specified sections help
    Resolved issues related to URL encoding which fixes being unable to
    access files (404 error).
    
    With HTML5 Mode set to true, all <embed> tags that are adding video must
    be changed to <video></video> Note: add 'controls' attribute to the
    video tag to enable video controls.
    
    HTML section partials must use ng-click=GoToSection() method in order to
    call the correct page. This method accepts a string "title" that
    corresponds to the title given in the app\contents\contents.json file.

--- app/contents/contents.js
+++ app/contents/contents.js
@@ -3,24 +3,30 @@
 angular.module('ngHelp.contents', [])
 
     .factory('ContentsService', function($timeout, $http) {
+        var data = {};
         var service = {
             fetch: function() {
+              // If we have not already retrieved the contents.JSON object, do so.
+              if(data !== null) {
                 return $timeout(function() {
                     console.log('making http request');
                     // var data = $http.get('/contents/contents.json?v=' + (new Date().getTime())).then(function(response) {
                     //     return response.data;
                     // });
-                    var data = $http.get('/contents/contents.json').then(function(response) {
+                    data = $http.get('/contents/contents.json').then(function(response) {
                         return response.data;
                     });
 
-
                     console.log("data: \n" + data);
                     return data;
-                }, 30);
+                  }, 30);
+                }
+                else {
+                  console.log("Hooray! Already have contents.json loaded!");
+                  return data;
+                }
+              }
             }
-        }
-
         return service;
     })
 
@@ -30,9 +36,20 @@ angular.module('ngHelp.contents', [])
         });
     }])
 
-    .controller('SectionController', ['$scope', '$route', '$routeParams', function($scope, $route, $routeParams) {
+    .controller('SectionController', ['$scope', '$location', 'ContentsService', function($scope, $location, ContentsService) {
         console.log("SectionController");
-        //$route.current.templateUrl = '/p/' + $routeParams.name + ".html?v=" + (new Date().getTime());
+        // $route.current.templateUrl = '/p/' + $routeParams.name + ".html?v=" + (new Date().getTime());
+        $scope.GoToSection = function(sectionTitle) {
+          // Get contents.json object
+          var contents = ContentsService.fetch().then(function(contents) {
+            for (var i = 0; i < contents.length; i++) {
+                if (contents[i].title === sectionTitle) {
+                    $location.url("/" + contents[i].url);
+                    console.log("GoToSection Path: " + $location.url());
+                }
+              }
+            });
+        };
     }])
 
     .directive('nghelpContents', function() {
